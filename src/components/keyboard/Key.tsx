ac15efdb801213490734289b9d2011c6d5c9cee4
Stephen <stephen.l.bell@gmail.com>
2022-01-17 14:59:04 -0500

    Implement rough version of Key

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -1,8 +1,17 @@
 
-export default function Key(): JSX.Element {
+import { Button } from "@mui/material";
+import { CharStatus } from "../../lib/status"
+import { KeyProps } from "../../lib/keyboard";
+
+export default function Key({ value, status, handleClick }: KeyProps): JSX.Element {
 
   return (
-    <>
-    </>
+    <Button
+      onClick={handleClick}
+      value={value}
+      variant="outlined"
+    >
+      {value}
+    </Button>
   );
 }
