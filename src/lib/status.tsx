176c28ab625d8d7c7162b79ab43e2b03e935b151
Stephen <stephen.l.bell@gmail.com>
2022-01-17 15:01:23 -0500

    Add default status to all characters

--- src/lib/status.tsx
+++ src/lib/status.tsx
@@ -1,1 +1,30 @@
 export type CharStatus = "CORRECT" | "INCORRECT" | "PRESENT" | "DEFAULT";
+
+export const keyStatuses = {
+  "A": "DEFAULT" as CharStatus,
+  "B": "DEFAULT" as CharStatus,
+  "C": "DEFAULT" as CharStatus,
+  "D": "DEFAULT" as CharStatus,
+  "E": "DEFAULT" as CharStatus,
+  "F": "DEFAULT" as CharStatus,
+  "G": "DEFAULT" as CharStatus,
+  "H": "DEFAULT" as CharStatus,
+  "I": "DEFAULT" as CharStatus,
+  "J": "DEFAULT" as CharStatus,
+  "K": "DEFAULT" as CharStatus,
+  "L": "DEFAULT" as CharStatus,
+  "M": "DEFAULT" as CharStatus,
+  "N": "DEFAULT" as CharStatus,
+  "O": "DEFAULT" as CharStatus,
+  "P": "DEFAULT" as CharStatus,
+  "Q": "DEFAULT" as CharStatus,
+  "R": "DEFAULT" as CharStatus,
+  "S": "DEFAULT" as CharStatus,
+  "T": "DEFAULT" as CharStatus,
+  "U": "DEFAULT" as CharStatus,
+  "V": "DEFAULT" as CharStatus,
+  "W": "DEFAULT" as CharStatus,
+  "X": "DEFAULT" as CharStatus,
+  "Y": "DEFAULT" as CharStatus,
+  "Z": "DEFAULT" as CharStatus
+}
