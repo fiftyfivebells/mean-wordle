64cbef980e14707924ae480481239cd253024bbf
Stephen <stephen.l.bell@gmail.com>
2022-01-17 20:58:41 -0500

    Add background color depending on status

--- src/components/grid/GridSquare.tsx
+++ src/components/grid/GridSquare.tsx
@@ -2,11 +2,28 @@ import { Box } from "@mui/material";
 import { Status } from "../../lib/status";
 
 export default function GridSquare({ value, status = "DEFAULT" }: Status): JSX.Element {
+
+  const styles = {
+    border: "solid 1px black",
+    width: "40px",
+    height: "40px",
+    backgroundColor: "white",
+    color: "white"
+  }
+
+  if (status === "CORRECT") {
+    styles.backgroundColor = "green"
+  } else if (status === "PRESENT") {
+    styles.backgroundColor = "yellow";
+  } else if (status === "INCORRECT") {
+    styles.backgroundColor = "gray";
+  } else {
+    styles.color = "black";
+  }
+  
   return (
-    <Box
-      sx={{ border: "solid 1px black", width: "40px", height: "40px" }}
-    >
-      {value}
+    <Box sx={styles}>
+      <strong>{value}</strong>
     </Box>
   );
 }
--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -5,8 +5,24 @@ import { KeyProps } from "../../lib/keyboard";
 
 export default function Key({ value, status, handleClick }: KeyProps): JSX.Element {
 
+  const styles = {
+    backgroundColor: "white",
+    color: "white"
+  };
+
+  if (status === "CORRECT") {
+    styles.backgroundColor = "green";
+  } else if (status === "PRESENT") {
+    styles.backgroundColor = "yellow";
+  } else if (status === "INCORRECT") {
+    styles.backgroundColor = "gray";
+  } else {
+    styles.color = "black";
+  }
+  
   return (
     <Button
+      sx={styles}
       onClick={handleClick}
       value={value}
       variant="outlined"
