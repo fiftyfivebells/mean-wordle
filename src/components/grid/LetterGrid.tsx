bf80c9319be5cb75f9789386bf167ce380323240
Stephen <stephen.l.bell@gmail.com>
2022-01-20 19:55:41 -0500

    Reformat file using prettier

--- src/components/grid/LetterGrid.tsx
+++ src/components/grid/LetterGrid.tsx
@@ -1,18 +1,24 @@
 import { Grid, Stack } from "@mui/material";
-import GridRow from "./GridRow"
-import { CharStatus, Status } from "../../lib/status"
+import GridRow from "./GridRow";
+import { CharStatus, Status } from "../../lib/status";
 
-export default function LetterGrid({ currentGuess, guesses, keyStatus }:
-  { currentGuess: string, guesses: Status[][], keyStatus: { [value: string]: CharStatus } }): JSX.Element {
-
+export default function LetterGrid({
+  currentGuess,
+  guesses,
+  keyStatus,
+}: {
+  currentGuess: string;
+  guesses: Status[][];
+  keyStatus: { [value: string]: CharStatus };
+}): JSX.Element {
   function addStatusToCurrentGuess(guess: string): Status[] {
     const guessStatus: Status[] = [];
 
     guess.split("").forEach((char) => {
       const key = {
         value: char,
-        status: "DEFAULT" as CharStatus
-      }
+        status: "DEFAULT" as CharStatus,
+      };
       guessStatus.push(key);
     });
 
@@ -20,19 +26,21 @@ export default function LetterGrid({ currentGuess, guesses, keyStatus }:
   }
 
   const completedRows: JSX.Element[] = guesses.map((guess, i) => {
-    return <GridRow key={i} characters={guess} />
+    return <GridRow key={i} characters={guess} />;
   });
   const empties: number = 5 - guesses.length;
-  const emptyRows = Array(empties).fill(0).map((_, i) => <GridRow key={i} />);
+  const emptyRows =
+    empties === -1
+      ? []
+      : Array(empties)
+          .fill(0)
+          .map((_, i) => <GridRow key={i} />);
 
   return (
-    <Stack
-      direction="column"
-      alignItems="center"
-    >
+    <Stack direction="column" alignItems="center">
       {completedRows}
-      <GridRow characters={addStatusToCurrentGuess(currentGuess)} />
-      {emptyRows}
+      {guesses.length < 6 && <GridRow characters={addStatusToCurrentGuess(currentGuess)} />}
+      {(empties > -1) && emptyRows}
     </Stack>
   );
 }
