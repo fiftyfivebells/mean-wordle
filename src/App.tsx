78d8783553e7deb62a26b7faf0d354c504c3daff
Stephen <stephen.l.bell@gmail.com>
2022-01-20 20:05:00 -0500

    Add state to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 function App() {
+  const [isNotAWordModalOpen, setIsNotAWordModalOpen] =
+    React.useState<boolean>(false);
+  const [isWinModalOpen, setIsWinModalOpen] = React.useState(false);
+  const [isGameWon, setIsGameWon] = React.useState(false);
+  const [currentGuess, setCurrentGuess] = React.useState("");
+  const [guesses, setGuesses] = React.useState(Array<Status[]>());
+  const [keyStatus, setKeyStatus] =
+    React.useState<{ [key: string]: CharStatus }>(keyStatuses);
   return (
     <>
     </>
