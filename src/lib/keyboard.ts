14b1ba19ddeaccee0e88084afdde8e0fdbee45c1
Stephen <stephen.l.bell@gmail.com>
2022-01-20 19:54:36 -0500

    Add the letters of the alphabet as a set

--- src/lib/keyboard.ts
+++ src/lib/keyboard.ts
@@ -13,3 +13,32 @@ export interface KeyboardProps {
   handleValue: (e?: React.MouseEvent | null, key?: string) => void;
   keyStatus: { [key: string]: CharStatus };
 }
+
+export const ALPHABET = new Set([
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+]);
