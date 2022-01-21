89b2f46d9400ea39399c958c153a4699d4c1e7d5
Stephen <stephen.l.bell@gmail.com>
2022-01-20 19:49:57 -0500

    Update KeyProps interface

--- src/lib/keyboard.ts
+++ src/lib/keyboard.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CharStatus } from "./status";
 
 export interface KeyProps {
-  value: string,
-  status: CharStatus
-  handleClick: React.MouseEventHandler<HTMLButtonElement>
+  value: string;
+  status: CharStatus;
+  handleClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface KeyboardProps {
