fa4544e1ce61f0cede3a2fd42364f2e6767831bf
Stephen <stephen.l.bell@gmail.com>
2022-01-23 22:48:11 -0500

    Change function to get word based on date

--- src/lib/wordUtils.ts
+++ src/lib/wordUtils.ts
@@ -1,7 +1,15 @@
 import { fiveLetterWords, fiveLetterWordSet } from "../constants/wordlist";
 
-export function getWordOfDay(): string {
-  return fiveLetterWords[0];
+export function getWordOfDay(): {word: string, index: number} {
+  const epochMs: number = 1641013200000
+  const now = Date.now()
+  const msInDay = 86400000
+  const index = Math.floor((now - epochMs) / msInDay)
+  
+  return {
+    word: fiveLetterWords[index].toUpperCase(),
+    index: index 
+  }
 }
 
 export function isWordInWordList(word: string): boolean {
